93f568d068b3322c360ebccb5f15bd5717cae016
mylek <mylekspam@gmail.com>
2016-06-02 19:51:59 +0200

    usuneicie wierszy

--- class/DataDB.php
+++ class/DataDB.php
@@ -7,8 +7,6 @@ class DataDB extends Data implements DataDriver {
     public function getData() {
         $this->data = array('db' => array(
            'Wiersz 1',
-           'Wiersz 2',
-           'Wiersz 3',
         ));
     }
 }
