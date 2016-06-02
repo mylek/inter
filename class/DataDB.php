e41c15c7b2e677d3d181607576ee26346fee627f
mylek <mylekspam@gmail.com>
2016-06-02 19:47:56 +0200

    DataDB.php

---
+++ class/DataDB.php
@@ -0,0 +1,15 @@
+<?php
+
+include("DataDriver.php");
+include("Data.php");
+
+class DataDB extends Data implements DataDriver {
+    public function getData() {
+        $this->data = array('db' => array(
+           'Wiersz 1',
+           'Wiersz 2',
+           'Wiersz 3',
+        ));
+    }
+}
+
